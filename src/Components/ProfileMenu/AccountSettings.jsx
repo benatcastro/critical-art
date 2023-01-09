f0a39259ccbeafcdf9336fa526820ad7ccbf66d6
becastro <benatcastro@gmail.com>
2023-01-09 19:32:20 +0100

    avatar centered

--- src/Components/ProfileMenu/AccountSettings.jsx
+++ src/Components/ProfileMenu/AccountSettings.jsx
@@ -17,7 +17,6 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import { useForm } from "react-hook-form";
 import { createBrowserHistory } from "@remix-run/router";
-import { Auth } from "aws-amplify";
 
 const saveChanges = (data) => {};
 
@@ -61,7 +60,7 @@ export const AccountSettings = () => {
     register,
     control,
     handleSubmit,
-	defaultValues,
+    defaultValues,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(validationSchema),
@@ -69,134 +68,115 @@ export const AccountSettings = () => {
 
   return (
     <Paper className="account-settings-container">
-      <div className="user-info-container">
-        <div className="user-data">
-          {typeof userData === "undefined"
-            ? waitForData && <CircularProgress className="loading-data" />
-            : userData.map((items, idx) => {
-                return (
-                  <div className="update-data" key={0}>
-                    <div className="fullname" key={1}>
-                      <TextField
-                        label="First Name"
-                        key={idx}
-                        id="firstname"
-                        name="firstname"
-                        margin="normal"
-                        defaultValue={items.firstName}
-                        {...register("firstname")}
-                        error={errors.firstname ? true : false}
-                        helperText={
-                          errors.firstname ? errors.firstname.message : null
-                        }
-                        inputProps={{
-                          readOnly: readOnly ? true : false,
-                        }}
+      <div className="user-data">
+        {typeof userData === "undefined"
+          ? waitForData && <CircularProgress className="loading-data" />
+          : userData.map((items, idx) => {
+              return (
+                <div className="update-data" key={0}>
+                  <Box
+                    display="flex"
+                    width="100%"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <IconButton>
+                      <Avatar
+                        style={{ justifyContent: "center", display: "flex" }}
+                        sx={{ height: 128, width: 128 }}
+                        src={items.avatar}
+                        alt={items.username}
                       />
-                      <TextField
-                        key={2}
-                        label="Last Name"
-                        id="lastname"
-                        name="lastname"
-                        margin="normal"
-                        defaultValue={items.lastName}
-                        {...register("lastname")}
-                        error={errors.lastname ? true : false}
-                        helperText={
-                          errors.lastname ? errors.lastname.message : null
-                        }
-                        inputProps={{
-                          readOnly: readOnly ? true : false,
-                        }}
-                      />
-                    </div>
-                    <div className="user-email">
-                      <TextField
-                        key={3}
-                        label="Username"
-                        variant="outlined"
-                        id="username"
-                        name="username"
-                        margin="normal"
-                        defaultValue={items.username}
-                        {...register("username")}
-                        error={errors.username ? true : false}
-                        helperText={
-                          errors.username ? errors.username.message : null
-                        }
-                        inputProps={{
-                          readOnly: readOnly ? true : false,
-                        }}
-                      />
-                      <TextField
-                        key={4}
-                        label="Email"
-                        id="email"
-                        name="email"
-                        defaultValue={items.email}
-                        margin="normal"
-                        {...register("email")}
-                        error={errors.email ? true : false}
-                        helperText={errors.email ? errors.email.message : null}
-                        inputProps={{
-                          readOnly: readOnly ? true : false,
-                        }}
-                      />
-                    </div>
+                    </IconButton>
+                  </Box>
+                  <div className="fullname" key={1}>
                     <TextField
-                      key={5}
-                      label="Biography"
-                      multiline
-                      rows={4}
-                      id="bio"
-                      name="bio"
-                      fullWidth
-                      defaultValue={items.biography}
+                      label="First Name"
+                      key={idx}
+                      id="firstname"
+                      name="firstname"
                       margin="normal"
-                      {...register("bio")}
-                      error={errors.bio ? true : false}
-                      helperText={errors.bio ? errors.bio.message : null}
+                      defaultValue={items.firstName}
+                      {...register("firstname")}
+                      error={errors.firstname ? true : false}
+                      helperText={
+                        errors.firstname ? errors.firstname.message : null
+                      }
+                      inputProps={{
+                        readOnly: readOnly ? true : false,
+                      }}
+                    />
+                    <TextField
+                      key={2}
+                      label="Last Name"
+                      id="lastname"
+                      name="lastname"
+                      margin="normal"
+                      defaultValue={items.lastName}
+                      {...register("lastname")}
+                      error={errors.lastname ? true : false}
+                      helperText={
+                        errors.lastname ? errors.lastname.message : null
+                      }
                       inputProps={{
                         readOnly: readOnly ? true : false,
                       }}
                     />
                   </div>
-                );
-              })}
-          <div className="update-btn">
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={
-                readOnly
-                  ? () => setWrite(!readOnly)
-                  : () => handleSubmit(onSubmit)
-              }
-            >
-              <Typography color="basics.white">
-                {readOnly ? "Edit" : "Save"}
-              </Typography>
-            </Button>
-            {!readOnly ? (
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => history.go(0)}
-              >
-                <Typography color="basics.white">Cancel</Typography>
-              </Button>
-            ) : null}
-          </div>
-        </div>
-        <div className="avatar-container">
-          <IconButton onClick={() => setPopper(!openPopper)}>
-            <Avatar
-              sx={{ bgColor: "secondary.dark", width: 128, height: 128 }}
-              className="avatar"
-              id="avatar"
-            ></Avatar>
-          </IconButton>
-        </div>
+                  <div className="user-email">
+                    <TextField
+                      key={3}
+                      label="Username"
+                      variant="outlined"
+                      id="username"
+                      name="username"
+                      margin="normal"
+                      defaultValue={items.username}
+                      {...register("username")}
+                      error={errors.username ? true : false}
+                      helperText={
+                        errors.username ? errors.username.message : null
+                      }
+                      inputProps={{
+                        readOnly: readOnly ? true : false,
+                      }}
+                    />
+                    <TextField
+                      key={4}
+                      label="Email"
+                      id="email"
+                      name="email"
+                      defaultValue={items.email}
+                      margin="normal"
+                      {...register("email")}
+                      error={errors.email ? true : false}
+                      helperText={errors.email ? errors.email.message : null}
+                      inputProps={{
+                        readOnly: readOnly ? true : false,
+                      }}
+                    />
+                  </div>
+                  <TextField
+                    key={5}
+                    label="Biography"
+                    multiline
+                    rows={4}
+                    id="bio"
+                    name="bio"
+                    fullWidth
+                    defaultValue={items.biography}
+                    margin="normal"
+                    {...register("bio")}
+                    error={errors.bio ? true : false}
+                    helperText={errors.bio ? errors.bio.message : null}
+                    inputProps={{
+                      readOnly: readOnly ? true : false,
+                    }}
+                  />
+                  <div className="buttons"></div>
+                </div>
+              );
+            })}
       </div>
     </Paper>
   );
