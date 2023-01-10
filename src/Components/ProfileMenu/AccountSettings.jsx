e2e7a1b2ca018714744bfbc95394dc313963044c
becastro <benatcastro@gmail.com>
2023-01-10 06:49:14 +0100

    added validation

--- src/Components/ProfileMenu/AccountSettings.jsx
+++ src/Components/ProfileMenu/AccountSettings.jsx
@@ -39,17 +39,32 @@ export const AccountSettings = () => {
     resolveUserdata();
   }, []);
 
+  const validationSchema = Yup.object().shape({
+    firstname: Yup.string().required("First name is required"),
+    lastname: Yup.string().required("Last name is required"),
+    username: Yup.string()
+      .required("Username is required")
+      .min(4, "Username must be at least 6 characters")
+      .max(20, "Username must not exceed 20 characters"),
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    bio: Yup.string()
+      .max(100, "Biography must not exceed 100 characters"),
+  });
   const {
     register,
     control,
     handleSubmit,
     setValue,
     formState: { errors },
-  } = useForm({});
+  } = useForm({
+	resolver: yupResolver(validationSchema)
+  });
+  const onSubmit = (data) => console.log(data);
   if (!loadingUserData) {
-    return userData.map((items, index) => {
+    return userData.map((items) => {
       return (
         <Paper
+		key={0}
           className="account-settings-contair"
           style={{
             position: "absolute",
@@ -60,13 +75,15 @@ export const AccountSettings = () => {
           }}
         >
           <Box
+		  key={1}
             display="flex"
             width="100%"
             alignItems="center"
             justifyContent="center"
           >
-            <IconButton>
+            <IconButton key={2}>
               <Avatar
+			  key={3}
                 style={{ justifyContent: "center", display: "flex" }}
                 sx={{ height: 128, width: 128 }}
                 src={userData.avatar}
@@ -74,50 +91,84 @@ export const AccountSettings = () => {
               />
             </IconButton>
           </Box>
-          <Box ml="5%" mr="5%">
+          <Box ml="5%" mr="5%" key={4}>
             <Grid
+			key={5}
               container
               justifyContent="center"
               columnSpacing={{ xs: 1, sm: 2, md: 3 }}
               rowSpacing={2}
             >
-              <Grid item xs={6}>
+              <Grid item xs={6} key={6}>
                 <Typography color="basics.black">First Name</Typography>
                 <TextField
-                  key={5}
-                  id="bio"
-                  name="bio"
+                  id="firstname"
+                  name="firstname"
                   fullWidth
                   defaultValue={items.firstName}
                   {...register("firstname")}
-                  error={errors.bio ? true : false}
-                  helperText={errors.bio ? errors.bio.message : null}
+                  error={errors.lastname ? true : false}
+                  helperText={
+                    errors.firstname ? errors.firstname.message : null
+                  }
                   inputProps={{
                     readOnly: readOnly ? true : false,
                   }}
                 ></TextField>
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={6} key={7}>
                 <Typography color="basics.black">Last Name</Typography>
-                <TextField fullWidth></TextField>
+                <TextField
+                  id="lastname"
+                  name="lastname"
+                  fullWidth
+                  defaultValue={items.lastName}
+                  {...register("lastname")}
+                  error={errors.lastname ? true : false}
+                  helperText={errors.lastName ? errors.lastName.message : null}
+                  inputProps={{
+                    readOnly: readOnly ? true : false,
+                  }}
+                ></TextField>
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={6} key={8}>
                 <Typography color="basics.black">Username</Typography>
-                <TextField fullWidth></TextField>
+                <TextField
+                  id="username"
+                  name="username"
+                  fullWidth
+                  defaultValue={items.firstName}
+                  {...register("username")}
+                  error={errors.username ? true : false}
+                  helperText={errors.username ? errors.username.message : null}
+                  inputProps={{
+                    readOnly: readOnly ? true : false,
+                  }}
+                ></TextField>
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={6} key={9}>
                 <Typography color="basics.black">Email</Typography>
-                <TextField fullWidth></TextField>
+                <TextField
+                  id="email"
+                  name="email"
+                  fullWidth
+                  defaultValue={items.email}
+                  {...register("email")}
+                  error={errors.email ? true : false}
+                  helperText={errors.email ? errors.email.message : null}
+                  inputProps={{
+                    readOnly: readOnly ? true : false,
+                  }}
+                ></TextField>
               </Grid>
-              <Grid item xs={6}>
+              <Grid container ml={3} key={10}>
                 <Typography color="basics.black">Biography</Typography>
                 <TextField
-                  key={5}
-                  label="Biography"
                   multiline
                   rows={4}
                   id="bio"
                   name="bio"
+                  fullWidth
                   defaultValue={items.biography}
                   margin="normal"
                   {...register("bio")}
@@ -129,6 +180,7 @@ export const AccountSettings = () => {
                 />
               </Grid>
             </Grid>
+            <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
           </Box>
         </Paper>
       );
