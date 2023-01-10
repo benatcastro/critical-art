2360895f4717234e9e742a73fa4f90ff03d32afc
becastro <benatcastro@gmail.com>
2023-01-10 05:44:47 +0100

    added bio textfield

--- src/Components/ProfileMenu/AccountSettings.jsx
+++ src/Components/ProfileMenu/AccountSettings.jsx
@@ -47,50 +47,92 @@ export const AccountSettings = () => {
     formState: { errors },
   } = useForm({});
   if (!loadingUserData) {
-    return (
-      <Paper className="account-settings-contair" style={{position: "absolute", left: "50%", top: "60%", transform: "translate(-50%, -50%)", paddingBottom: "2%"}}>
-        <Box
-          display="flex"
-          width="100%"
-          alignItems="center"
-          justifyContent="center"
+    return userData.map((items, index) => {
+      return (
+        <Paper
+          className="account-settings-contair"
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "60%",
+            transform: "translate(-50%, -50%)",
+            paddingBottom: "2%",
+          }}
         >
-          <IconButton>
-            <Avatar
-              style={{ justifyContent: "center", display: "flex" }}
-              sx={{ height: 128, width: 128 }}
-              src={userData.avatar}
-              alt={userData.username}
-            />
-          </IconButton>
-        </Box>
-        <Box ml="5%" mr="5%">
-          <Grid
-            container
+          <Box
+            display="flex"
+            width="100%"
+            alignItems="center"
             justifyContent="center"
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-			rowSpacing={2}
           >
-            <Grid item xs={6}>
-              <Typography color="basics.black">First Name</Typography>
-              <TextField fullWidth></TextField>
+            <IconButton>
+              <Avatar
+                style={{ justifyContent: "center", display: "flex" }}
+                sx={{ height: 128, width: 128 }}
+                src={userData.avatar}
+                alt={userData.username}
+              />
+            </IconButton>
+          </Box>
+          <Box ml="5%" mr="5%">
+            <Grid
+              container
+              justifyContent="center"
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              rowSpacing={2}
+            >
+              <Grid item xs={6}>
+                <Typography color="basics.black">First Name</Typography>
+                <TextField
+                  key={5}
+                  id="bio"
+                  name="bio"
+                  fullWidth
+                  defaultValue={items.firstName}
+                  {...register("firstname")}
+                  error={errors.bio ? true : false}
+                  helperText={errors.bio ? errors.bio.message : null}
+                  inputProps={{
+                    readOnly: readOnly ? true : false,
+                  }}
+                ></TextField>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography color="basics.black">Last Name</Typography>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography color="basics.black">Username</Typography>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography color="basics.black">Email</Typography>
+                <TextField fullWidth></TextField>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography color="basics.black">Biography</Typography>
+                <TextField
+                  key={5}
+                  label="Biography"
+                  multiline
+                  rows={4}
+                  id="bio"
+                  name="bio"
+                  defaultValue={items.biography}
+                  margin="normal"
+                  {...register("bio")}
+                  error={errors.bio ? true : false}
+                  helperText={errors.bio ? errors.bio.message : null}
+                  inputProps={{
+                    readOnly: readOnly ? true : false,
+                  }}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <Typography color="basics.black">Last Name</Typography>
-              <TextField fullWidth></TextField>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography color="basics.black">Username</Typography>
-              <TextField fullWidth></TextField>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography color="basics.black">Email</Typography>
-              <TextField fullWidth></TextField>
-            </Grid>
-          </Grid>
-        </Box>
-      </Paper>
-    );
+          </Box>
+        </Paper>
+      );
+    });
   }
 };
 /* */
