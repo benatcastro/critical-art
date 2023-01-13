bb7e0421b749a89c602435cdb4a0a01e16c45201
becastro <benatcastro@gmail.com>
2023-01-13 05:05:39 +0100

    images centered

--- src/Components/ProfileMenu/ImageSettings.jsx
+++ src/Components/ProfileMenu/ImageSettings.jsx
@@ -2,6 +2,7 @@ import { API, Auth, Storage } from "aws-amplify";
 import { useState, useEffect } from "react";
 import { imageByAuth } from "../../graphql/queries";
 import { Paper } from "@mui/material";
+import { Box } from "@mui/system";
 
 export const ImageSettings = () => {
   const [Images, setImages] = useState([]);
@@ -20,7 +21,6 @@ export const ImageSettings = () => {
       imageList.map(async (image) => {
         image.src = image.src.substring(image.src.indexOf("=") + 1);
         image.src = image.src.substring(0, image.src.length - 1);
-        console.log("before",image);
         const url = await Storage.get(image.src);
         image.src = url;
         return image;
@@ -35,7 +35,25 @@ export const ImageSettings = () => {
       (
         <Paper elevation={3} style={{ paddingBottom: "2%" }}>
           {Images.map((image, index) => {
-            return <img src={image.src} />;
+            return (
+              <Box
+                key={index}
+                width="100%"
+                display="flex"
+                alignContent="center"
+                alignItems="center"
+              >
+                <img
+                  src={image.src}
+                  alt=""
+                  key={index}
+                  style={{
+                    width: "75%",
+                    margin: "2% auto",
+                  }}
+                />
+              </Box>
+            );
           })}
         </Paper>
       )
